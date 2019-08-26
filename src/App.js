b3276cdafe933ca2559e56c2c0c8ba9442f716aa
Moisés S E Aguilar <moisesmagno@gmail.com>
2019-08-26 10:55:12 -0300

    Terminado o projeto de contador de voltas.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+
+import MostrarVoltas from './MostrarVoltas';
+import MostraTempo from './MostraTempo';
+import Button from './Button';
 
 function App() {
+
+  const [numVoltas, setnumVoltas] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [tempo, setTempo] = useState(0);
+
+  const acresentarVoltas = () => {
+    
+    setnumVoltas(numVoltas + 1);
+  }
+
+  const decresentarVoltas = () => {
+    if(numVoltas > 0){
+      setnumVoltas(numVoltas - 1);
+    }
+  }
+
+  const reset = () => {
+    setnumVoltas(0);
+    setTempo(0);
+  }
+
+  useEffect(() => {
+    let timer = null;
+    if(running){
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    }
+  },[running]);
+  //Passando o colchetes vazias, o conteúdo será executado apenas uma vez.
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  }
+
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
+    <div>
+      <MostrarVoltas voltas={numVoltas}/>
+      <Button text='+' className='bigger' onClick={acresentarVoltas} />
+      <Button text='-' className='bigger' onClick={decresentarVoltas} />
+      {
+        numVoltas > 0 && 
+        <div>
+          <MostraTempo tempo={Math.round(tempo / numVoltas)} />
+          <Button onClick={toggleRunning} text={running ? 'Pausar' : 'Iniciar' } />
+          <Button onClick={reset} text='Reinicar' />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/MostrarVoltas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MostrarVoltas = (props) => {
+  return (
+    <p className="voltas">
+      <span >{props.voltas}</span>
+      <br/>
+      Voltas
+    </p>
+  );
+}
+
+export default MostrarVoltas;
